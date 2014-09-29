5a977f7ab2382b442beeeba277f272d660044b93
ZipFile <lin.aaa.lin@gmail.com>
2014-09-29 13:12:59 +0300

    Fix demo.py

--- demo.py
+++ demo.py
@@ -14,38 +14,38 @@ def main():
 	### change _USERNAME,_PASSWORD first!
 	#api.login("login", _USERNAME, _PASSWORD, 0)
 
-	#print ">>> ranking(male, day, page=1)"
+	#print(">>> ranking(male, day, page=1)")
 	#rank_list = api.ranking("all", 'day', 1)
 	#for img in rank_list:
-	#	print img
+	#	print(repr(img))
 
 	illust = api.get_illust(37006153)
 	print(">>> %d %d %s" % (illust.illust_id, illust.pages, illust.url))
 
-	#print ">>> get_member(1184799, page=1)"
+	#print(">>> get_member(1184799, page=1)")
 	#illust_list = api.get_member(428027, 1)
 	#for idx, img in enumerate(illust_list):
-	#	print "[%d] bookmarks=%d, %s" % (idx+1, img.cnt_bookmark, img)
+	#	print("[%d] bookmarks=%d, %s" % (idx+1, img.cnt_bookmark, img))
 	
 	### Authentication required! call api.login first!
-	#print ">>> get_bookmark(1418182, page=1)"
+	#print(">>> get_bookmark(1418182, page=1)")
 	#bookmark_list = api.get_bookmark(1418182, 1)
 	#for img in bookmark_list:
-	#	print img
+	#	print(repr(img))
 
-	#print ">>> ranking_log(2013-01-15, monthly, page=1)"
+	#print(">>> ranking_log(2013-01-15, monthly, page=1)")
 	#rank_list = api.ranking_log('2013',1,'monthly','05','15')
 	#for img in rank_list:
-	#	print img
+	#	print(repr(img))
 
-	#print ">>> get_user(level=3, id=1184799)"
+	#print(">>> get_user(level=3, id=1184799)")
 	#user = api.get_user(3, 1184799)
-	#print "%s: %s" % (user.dispname, user.pic)
+	#print("%s: %s" % (user.dispname, user.pic))
 
-	#print ">>> get_mypixiv_all(id=428027, p=1)"
+	#print(">>> get_mypixiv_all(id=428027, p=1)")
 	#user_list = api.get_mypixiv_all(428027, 1)
 	#for usr in user_list:
-	#	print usr
+	#	print(repr(usr))
 
 if __name__ == '__main__':
 	main()
