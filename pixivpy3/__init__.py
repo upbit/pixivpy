da01370aabeee68389b8c80f9561dbb3763e6b1b
xiyihan <71264788+xiyihan0@users.noreply.github.com>
2025-04-28 21:55:03 +0800

    Experimental: Add partial Web api implementation (with sync/async methods)

--- pixivpy3/__init__.py
+++ pixivpy3/__init__.py
@@ -3,5 +3,11 @@
 from .aapi import AppPixivAPI
 from .bapi import ByPassSniApi
 from .utils import PixivError
+from .webapi import WebPixivAPI  # Import the new class
 
-__all__ = ("AppPixivAPI", "ByPassSniApi", "PixivError")
+__all__ = (
+    "AppPixivAPI",
+    "ByPassSniApi",
+    "PixivError",
+    "WebPixivAPI",
+)  # Add the new class to __all__
